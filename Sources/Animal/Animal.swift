6415b518e732415a5b163f6f0d1c2b21d68b6e2e
YoshikiTsukada <tsukadayyy@gmail.com>
2020-07-19 00:23:07 +0900

    Fix build error

--- Sources/Animal/Animal.swift
+++ Sources/Animal/Animal.swift
@@ -1,4 +1,9 @@
 open class Animal {
     public var name: String
     public var ownerName: String
+
+    public init(name: String, ownerName: String) {
+        self.name = name
+        self.ownerName = ownerName
+    }
 }
--- Tests/AnimalTests/AnimalTests.swift
+++ Tests/AnimalTests/AnimalTests.swift
@@ -6,7 +6,7 @@ final class AnimalTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Animal().text, "Hello, World!")
+//        XCTAssertEqual(Animal().text, "Hello, World!")
     }
 
     static var allTests = [
